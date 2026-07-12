7afc60c271f729ec7936ebe7eef6cac4c2b658cb
Patrick Thomas <patrickthomas101x@gmail.com>
2026-07-11 20:21:27 -0500

    Declare members

--- include/collections/array.hpp
+++ include/collections/array.hpp
@@ -6,9 +6,10 @@
 #include <cstring>
 
 // ISO C++ Includes
+#include <algorithm>
 #include <initializer_list>
 #include <type_traits>
-#include <algorithm>
+#include <stdexcept>
 
 namespace collections {
     template<typename T, std::size_t N>
@@ -34,26 +35,301 @@ namespace collections {
         value_type values_[N];
 
     public:
-        // ── iterator ────────────────────────────────────────────────────────────────────────────────────────────────
+        // ── iterator ─────────────────────────────────────────────────────────────────────────────────────────────────
+        class iterator {
+        public:
+            // ── Type Definitions ─────────────────────────────────────────────────────────────────────────────────────
+            using iterator_category = std::contiguous_iterator_tag;
 
-        // ── const_iterator ──────────────────────────────────────────────────────────────────────────────────────────
+            using value_type = array::value_type;
 
+            using size_type = array::size_type;
+
+            using difference_type = array::difference_type;
+
+            using pointer = array::pointer;
+
+            using const_pointer = array::const_pointer;
+
+            using reference = array::reference;
+
+            using const_reference = array::const_reference;
+
+        private:
+            // ── Friends ──────────────────────────────────────────────────────────────────────────────────────────────
+            friend class array;
+
+            // ── Fields ───────────────────────────────────────────────────────────────────────────────────────────────
+            pointer ptr_;
+
+        public:
+            // ── Constructors ─────────────────────────────────────────────────────────────────────────────────────────
+            inline constexpr iterator() noexcept;
+
+            inline constexpr iterator(const iterator& other) noexcept;
+
+            inline constexpr iterator(iterator&& other) noexcept;
+
+            // ── Destructor ───────────────────────────────────────────────────────────────────────────────────────────
+            inline constexpr ~iterator() noexcept = default;
+
+            // ── Overload Operators ───────────────────────────────────────────────────────────────────────────────────
+            inline constexpr iterator& operator=(const iterator& lhs) noexcept {
+
+            }
+
+            inline constexpr iterator& operator=(iterator&& lhs) noexcept {
+
+            }
+
+            [[nodiscard]] inline constexpr bool operator==(const iterator& lhs) const noexcept {
+
+            }
+
+            [[nodiscard]] inline constexpr auto operator<=>(const iterator& lhs) const noexcept {
+
+            }
+
+            inline constexpr iterator& operator++() noexcept {
+
+            }
+
+            inline constexpr iterator operator++(int) noexcept {
+
+            }
+
+            inline constexpr iterator& operator--() noexcept {
+
+            }
+
+            inline constexpr iterator operator--(int) noexcept {
+
+            }
+
+            inline constexpr iterator& operator+=(const difference_type n) noexcept {
+
+            }
+
+            [[nodiscard]] inline constexpr iterator operator+(const difference_type n) const noexcept {
+
+            }
+
+            inline constexpr iterator& operator-=(const difference_type n) noexcept {
+
+            }
+
+            [[nodiscard]] inline constexpr iterator operator-(const difference_type n) const noexcept {
+
+            }
+        };
+
+        // ── const_iterator ───────────────────────────────────────────────────────────────────────────────────────────
+        class const_iterator {
+        public:
+            // ── Type Definitions ─────────────────────────────────────────────────────────────────────────────────────
+            using iterator_category = std::contiguous_iterator_tag;
+
+            using value_type = array::value_type;
+
+            using size_type = array::size_type;
+
+            using difference_type = array::difference_type;
+
+            using pointer = array::pointer;
+
+            using const_pointer = array::const_pointer;
+
+            using reference = array::reference;
+
+            using const_reference = array::const_reference;
+
+        private:
+            // ── Friends ──────────────────────────────────────────────────────────────────────────────────────────────
+            friend class array;
+
+            // ── Fields ───────────────────────────────────────────────────────────────────────────────────────────────
+            pointer ptr_;
+
+            // ── Constructors ─────────────────────────────────────────────────────────────────────────────────────────
+            constexpr const_iterator() noexcept : ptr_(nullptr) {}
+        };
+
+        // ── reverse_iterator ─────────────────────────────────────────────────────────────────────────────────────────
+        class reverse_iterator {
+        public:
+            // ── Type Definitions ─────────────────────────────────────────────────────────────────────────────────────
+            using iterator_category = std::contiguous_iterator_tag;
+
+            using value_type = array::value_type;
+
+            using size_type = array::size_type;
+
+            using difference_type = array::difference_type;
+
+            using pointer = array::pointer;
+
+            using const_pointer = array::const_pointer;
+
+            using reference = array::reference;
+
+            using const_reference = array::const_reference;
+
+        private:
+            // ── Friends ──────────────────────────────────────────────────────────────────────────────────────────────
+            friend class array;
+
+            // ── Fields ───────────────────────────────────────────────────────────────────────────────────────────────
+            pointer ptr_;
+        };
+
+        // ── const_reverse_iterator ───────────────────────────────────────────────────────────────────────────────────
+        class const_reverse_iterator {
+        public:
+            // ── Type Definitions ─────────────────────────────────────────────────────────────────────────────────────
+            using iterator_category = std::contiguous_iterator_tag;
+
+            using value_type = array::value_type;
+
+            using size_type = array::size_type;
+
+            using difference_type = array::difference_type;
+
+            using pointer = array::pointer;
+
+            using const_pointer = array::const_pointer;
+
+            using reference = array::reference;
+
+            using const_reference = array::const_reference;
+
+        private:
+            // ── Friends ──────────────────────────────────────────────────────────────────────────────────────────────
+            friend class array;
+
+            // ── Fields ───────────────────────────────────────────────────────────────────────────────────────────────
+            pointer ptr_;
+        };
 
         // ── Constructors ─────────────────────────────────────────────────────────────────────────────────────────────
         inline constexpr array() noexcept = default;
 
-        // ── Overloaded Operators ─────────────────────────────────────────────────────────────────────────────────────
+        inline constexpr array(const array& other) noexcept;
 
+        inline constexpr array(array&& other) noexcept;
+
+        inline constexpr array(std::initializer_list<value_type> values) noexcept;
+
+        // ── Destructor ───────────────────────────────────────────────────────────────────────────────────────────────
+        inline constexpr ~array() noexcept = default;
+
+        // ── Overloaded Operators ─────────────────────────────────────────────────────────────────────────────────────
+        inline constexpr array& operator=(const array& lhs) noexcept {
+
+        }
+
+        inline constexpr array& operator=(array&& lhs) noexcept {
+
+        }
+
+        [[nodiscard]] constexpr bool operator==(const array& lhs) const noexcept {
+
+        }
+
+        [[nodiscard]] constexpr auto operator<=>(const array& lhs) const noexcept {
+
+        }
+
+        [[nodiscard]] inline constexpr reference operator[](const size_type index) {
+
+        }
+
+        [[nodiscard]] inline constexpr const_reference operator[](const size_type index) const {
+
+        }
 
         // ── Methods ──────────────────────────────────────────────────────────────────────────────────────────────────
+        [[nodiscard]] inline constexpr reference at(const size_type index) {
+
+        }
+
+        [[nodiscard]] inline constexpr const_reference at(const size_type index) const {
+
+        }
+
+        [[nodiscard]] inline constexpr reference front() {
+
+        }
+
+        [[nodiscard]] inline constexpr const_reference front() const {
+
+        }
+
+        [[nodiscard]] inline constexpr reference back() noexcept {
+
+        }
+
+        [[nodiscard]] inline constexpr const_reference back() const noexcept {
+
+        }
+
+        [[nodiscard]] inline constexpr pointer data() noexcept {
+
+        }
+
+        [[nodiscard]] inline constexpr const_pointer data() const noexcept {
+
+        }
+
+        [[nodiscard]] inline constexpr iterator begin() {
+
+        }
+
+        [[nodiscard]] inline constexpr iterator end() {
+
+        }
+
+        [[nodiscard]] inline constexpr const_iterator cbegin() const {
+
+        }
+
+        [[nodiscard]] inline constexpr const_iterator cend() const {
+
+        }
+
+        [[nodiscard]] inline constexpr reverse_iterator rbegin() {
+
+        }
+
+        [[nodiscard]] inline constexpr reverse_iterator rend() {
+
+        }
+
+        [[nodiscard]] inline constexpr const_reverse_iterator crbegin() const {
+
+        }
+
+        [[nodiscard]] inline constexpr const_reverse_iterator crend() const {
+
+        }
+
+        [[nodiscard]] inline constexpr bool empty() const {
+
+        }
+
+        [[nodiscard]] inline constexpr size_type size() const {
+
+        }
+
+        [[nodiscard]] inline constexpr size_type max_size() const {
+
+        }
+
+        inline constexpr void fill(const_reference value) noexcept {}
+
+        inline constexpr void swap(array& other) noexcept {
+
+        }
     };
-
-    // ── Constructors ─────────────────────────────────────────────────────────────────────────────────────────────────
-
-    // ── Overloaded Operators ─────────────────────────────────────────────────────────────────────────────────────────
-
-
-    // ── Methods ──────────────────────────────────────────────────────────────────────────────────────────────────────
 
 } // namespace collections
 
