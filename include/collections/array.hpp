358678992f059842cae431a49b94d242d90600c3
Patrick Thomas <patrickthomas101x@gmail.com>
2026-07-15 16:34:39 -0500

    Implement back() const overload

--- include/collections/array.hpp
+++ include/collections/array.hpp
@@ -128,7 +128,7 @@ namespace collections {
         }
 
         [[nodiscard]] inline constexpr const_reference back() const noexcept {
-
+            return this->values_[this->size() - 1];
         }
 
         [[nodiscard]] inline constexpr pointer data() noexcept {
