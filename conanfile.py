9fbf5a4f964aab6d833af4c627cc7368bec6e91a
Patrick Thomas <patrickthomas101x@gmail.com>
2026-07-17 17:17:56 -0500

    Make array_benchmarks seperate from array_tests

--- conanfile.py
+++ conanfile.py
@@ -3,41 +3,47 @@ from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
 import os
 
 class Array(ConanFile):
-    name = "collections-array"
-    version = "0.1.0"
-    author = "Patman1O1"
-    description = ""
+    name: str = "collections-array"
+    version: str = "0.1.0"
+    author: str = "Patman1O1"
+    description: str = ""
 
-    settings = ("os", "arch", "compiler", "build_type")
-    exports_sources = ("CMakeLists.txt", "cmake/*", "include/*", "tests/*")
+    settings: tuple[str] = ("os", "arch", "compiler", "build_type")
+    exports_sources: tuple[str] = ("CMakeLists.txt", "cmake/*", "include/*", "tests/*")
 
-    options = {
+    options: dict[str, list[bool]] = {
+        "build_all": [True, False],
+        "build_benchmarks": [True, False],
         "build_tests": [True, False],
-        "build_benchmarks": [True, False]
     }
 
-    default_options = {
+    default_options: dict[str, bool] = {
+        "build_all": False,
+        "build_benchmarks": False,
         "build_tests": False,
-        "build_benchmarks": False
     }
+
+    def configure(self) -> None:
+        if bool(self.options.build_all):
+            self.options.build_benchmarks = True
+            self.options.build_tests = True
+        elif self.settings.build_type == "Debug":
+            self.options.build_tests = True
 
     def build_requirements(self) -> None:
         self.tool_requires("cmake/[>=4.3.0]")
-        if bool(self.options.build_tests) or self.settings.build_type == "Debug":
-            self.test_requires("gtest/1.17.0")
-        elif bool(self.options.build_benchmarks):
+
+        if bool(self.options.build_benchmarks):
             self.test_requires("benchmark/[>=1.9.5]")
 
+        if bool(self.options.build_tests) or self.settings.build_type == "Debug":
+            self.test_requires("gtest/1.17.0")
 
-    def layout(self) -> None:
-        cmake_layout(self)
+    def layout(self) -> None: cmake_layout(self)
 
     def generate(self) -> None:
         toolchain = CMakeToolchain(self)
-        if self.settings.build_type == "Debug":
-            toolchain.variables["BUILD_TESTS"] = True
-        else:
-            toolchain.variables["BUILD_TESTS"] = bool(self.options.build_tests)
+        toolchain.variables["BUILD_TESTS"] = bool(self.options.build_tests)
         toolchain.variables["BUILD_BENCHMARKS"] = bool(self.options.build_benchmarks)
         toolchain.generate()
         CMakeDeps(self).generate()
@@ -47,8 +53,7 @@ class Array(ConanFile):
         cmake.configure()
         cmake.build()
 
-    def package(self) -> None:
-        CMake(self).install()
+    def package(self) -> None: CMake(self).install()
 
     def package_info(self) -> None:
         self.cpp_info.set_property("cmake_target_name", "collections::array")
