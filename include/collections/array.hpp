a3b9cac4a7193fa9f106e533181684fa8dac0b98
Patrick Thomas <patrickthomas101x@gmail.com>
2026-07-25 16:03:41 -0500

    Add Methods divider

--- include/collections/array.hpp
+++ include/collections/array.hpp
@@ -3438,6 +3438,7 @@ namespace collections {
             return this->values_[index];
         }
 
+        // ── Methods ──────────────────────────────────────────────────────────────────────────────────
         [[nodiscard]] constexpr auto at(const size_type index) -> reference {
             if (index >= N) [[unlikely]] {
                 throw std::out_of_range("collections::array::at index out of range");
