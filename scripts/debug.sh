3d021db0b790ebf76beb9d1c602fcc9bc0ab3cba
Patrick Thomas <patrickthomas101x@gmail.com>
2026-08-24 14:43:46 -0500

    Edit debug.sh

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -4,5 +4,4 @@ set -euo pipefail
 ROOT="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)/.."
 
 conan build "${ROOT}" -o "&:build_tests=True" -s build_type=Debug
-gdb "${ROOT}/build/Debug/bin/circular_array_run_time_test"
-
+gdb "${ROOT}/build/Debug/bin/array_test"
