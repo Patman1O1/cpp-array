ffe85a2c0c9eedae2181514248ee783d5dbc5df2
Patrick Thomas <patrickthomas101x@gmail.com>
2026-08-24 14:42:54 -0500

    Add scripts

---
+++ scripts/debug.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+ROOT="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)/.."
+
+conan build "${ROOT}" -o "&:build_tests=True" -s build_type=Debug
+gdb "${ROOT}/build/Debug/bin/circular_array_run_time_test"
+
